9afd027c13e7dfe7e3ca5627d19d2d313fc8b1f0
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-05-14 08:52:32 +0200

    Add backend framework, Add auth module

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const app = (fastify = require('fastify')({
+  logger: { level: 'warn' },
+}));
+
+app.register(require('fastify-swagger'), {
+  routePrefix: '/docs',
+  exposeRoute: true,
+
+  openapi: {
+    info: {
+      title: 'Homare - API Service',
+      description: '🌈 Application for building and sharing project color schemes.',
+      version: '0.1.0',
+    },
+    tags: [
+      { name: 'Users', description: '👱 User management endpoints.' },
+      { name: 'Auth', description: '🔒 User authentication endpoints.' },
+    ],
+    servers: [
+      { url: 'http://127.0.0.1:3000', description: 'Development' },
+      { url: 'https://homare.bednarski.dev/api', description: 'Production' },
+    ],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+        },
+      },
+    },
+  },
+});
+
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+
+app.register(users, { prefix: '/users' });
+app.register(auth, { prefix: '/auth' });
+
+module.exports = app;
