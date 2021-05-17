15dcf455909651e1d4be6a7d3cec7ecca9438fb8
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-05-17 12:06:29 +0200

    Add Landing page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div> </div>
+  <div class="flex flex-col items-center justify-center w-full h-full">
+    <div class="container flex flex-col items-center p-5 lg:flex-row lg:p-32">
+      <div class="flex flex-col m-8 lg:flex-grow">
+        <h1 class="mb-6 text-2xl font-bold tracking-tight font-heading lg:text-6xl text-gray">The simplest way to manage colors.</h1>
+        <p class="mb-6 text-gray-light ">Create and manage color palettes between projects.</p>
+
+        <div class="flex flex-col lg:flex-row">
+          <button class="px-8 py-3 my-6 text-sm font-medium text-white rounded-xl lg:max-w-max bg-primary focus:outline-none" @click="$router.push('/generator')"
+            >Start Generator</button
+          >
+          <button class="px-8 py-3 my-6 text-sm font-medium text-primary lg:ml-3 rounded-xl lg:max-w-max focus:outline-none" @click="$router.push('/auth')">Or, Sign In</button>
+        </div>
+      </div>
+
+      <div class="w-full lg:w-5/6 lg:max-w-lg">
+        <img class="object-cover object-center rounded-lg " alt="hero" src="https://dummyimage.com/720x600/F3F4F7/8693ac" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
