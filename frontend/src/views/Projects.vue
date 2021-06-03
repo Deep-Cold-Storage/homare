a90ecb0c0ccccfb719923afa63e98cb201fa754f
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-06-03 15:23:44 +0200

    Add projects

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -4,6 +4,7 @@
 
     <div class="flex flex-row w-full h-full py-4">
       <section class="hidden w-full max-w-xs p-5 lg:block ">
+        <!-- Owned Projects -->
         <p class="text-xs text-gray-light">Owned Projects</p>
         <button class="block px-8 py-3 my-6 text-sm font-medium text-center text-white rounded-xl lg:max-w-max bg-primary focus:outline-none">Create Project</button>
 
@@ -17,6 +18,7 @@
           <p class="text-xs text-gray-light">{{ item.description }}</p>
         </div>
 
+        <!-- Shared Projects -->
         <template v-if="projects.shared.length">
           <p class="text-xs text-gray-light">Shared with You</p>
 
@@ -28,20 +30,39 @@
       </section>
 
       <section class="flex flex-col w-full p-5">
-        <div class="flex flex-row justify-between">
+        <div class="flex flex-col lg:flex-row lg:justify-between">
+          <!-- Project -->
           <div>
             <h1 class="text-base font-semibold font-heading text-gray">{{ activeProject.name }}</h1>
             <p class="my-2 text-sm text-gray-light">{{ activeProject.description }}</p>
           </div>
 
-          <div class="flex flex-row">
+          <!-- Project Menu -->
+          <div class="flex flex-row justify-center mt-3 lg:mt-0">
             <p class="m-3 text-sm text-gray-light">Edit</p>
             <p class="m-3 text-sm text-gray-light">Share (2 Members)</p>
             <p class="m-3 text-sm text-gray-light">Delete</p>
           </div>
         </div>
 
-        <section> </section>
+        <section>
+          <!-- Palette -->
+          <div class="p-3 my-3 bg-white rounded shadow-sm cursor-pointer lg:p-5 lg:rounded-xl" v-for="(item, index) in activeProject.palettes" :key="index">
+            <h1 class="mb-2 text-sm font-medium font-heading ">{{ item.name }}</h1>
+
+            <!-- Colors -->
+            <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-8 2xl:grid-cols-12">
+              <div
+                class="flex flex-row items-center justify-between p-2 my-2 cursor-pointer lg:flex-col lg:p-5 lg:w-auto lg:rounded-xl"
+                v-for="(item, colorIndex) in item.colors"
+                :key="colorIndex"
+              >
+                <input v-model="activeProject.palettes[index].colors[colorIndex]" class="w-20 h-20 ml-3" type="color" />
+                <h1 class="m-3 mb-2 text-sm font-medium text-center font-heading ">{{ item }}</h1>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
     </div>
   </div>
@@ -68,6 +89,8 @@
           .get('/api/projects')
           .then((payload) => {
             this.projects = payload.data;
+
+            this.activeProject = this.projects.owned[0];
           })
           .catch((err) => {
             console.log(err);
